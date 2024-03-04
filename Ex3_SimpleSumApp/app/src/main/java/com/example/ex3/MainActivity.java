a4cbf342fffc7d7974de1423d6c365f174d7a003
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-03-04 15:05:43 +0700

    update Ex3

--- Ex3_SimpleSumApp/app/src/main/java/com/example/ex3/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/com/example/ex3/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Gắn layout tương ứng với file này
         setContentView(R.layout.activity_main);
     }
 }
