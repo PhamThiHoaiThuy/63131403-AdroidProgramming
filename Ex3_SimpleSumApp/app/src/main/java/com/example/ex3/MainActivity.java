29e78bcaf52a0cc5cca69155a4da7e4535dd80e8
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-03-07 21:46:52 +0700

    update TH3

--- Ex3_SimpleSumApp/app/src/main/java/com/example/ex3/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/com/example/ex3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ex3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //Gắn layout tương ứng với file này
         setContentView(R.layout.activity_main);
+    }
+
+    //đây là bộ lắng nghe và xử lí sự kiện tính tổng
+    public  void XuLyCong(View view){
+        //Tìm, tham chiếu điều khiển trên tập XML
+        EditText editTextSoA =  findViewById(R.id.edtA);
+        EditText editTextSoB =  findViewById(R.id.edtB);
+        EditText editTextKQ =  findViewById(R.id.edtKQ);
+
+        //lấy DL
+        String strA = editTextSoA.getText().toString();
+        String strB = editTextSoB.getText().toString();
+
+        //chuyển DL
+        int soA = Integer.parseInt(strA);
+        int soB = Integer.parseInt(strB);
+
+        //Tính tổng
+        int tong = soA + soB;
+        String strTong = String.valueOf(tong);
+
+        //hiện ra màn hình
+        editTextKQ.setText(strTong);
     }
 }
