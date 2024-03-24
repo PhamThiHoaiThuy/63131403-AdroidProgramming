f6099c8cc2918ca48e156f0a46ee5556b9cd4a7d
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-03-24 23:32:51 +0700

    Hoàn thành câu 1

--- Cau1_AppBMI/src/ntu/hoaithuy63131403/Cau1.java
+++ Cau1_AppBMI/src/ntu/hoaithuy63131403/Cau1.java
@@ -50,7 +50,7 @@ public class Cau1 extends JFrame {
 	 */
 	public Cau1() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 564, 617);
+		setBounds(100, 100, 536, 617);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(208, 232, 255));
 		contentPane.setForeground(new Color(208, 232, 255));
@@ -80,17 +80,17 @@ public class Cau1 extends JFrame {
 		edtWeigh.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("BMI của bạn: ");
-		lblNewLabel_2.setBounds(88, 331, 98, 29);
+		lblNewLabel_2.setBounds(88, 342, 98, 29);
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		contentPane.add(lblNewLabel_2);
 		
 		edtKq = new JTextField();
-		edtKq.setBounds(196, 333, 233, 29);
+		edtKq.setBounds(180, 344, 214, 29);
 		contentPane.add(edtKq);
 		edtKq.setColumns(10);
 		
 		Button btnKq = new Button("Kết quả");
-		btnKq.setBounds(133, 227, 118, 42);
+		btnKq.setBounds(122, 227, 118, 42);
 		btnKq.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnKq.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -111,9 +111,9 @@ public class Cau1 extends JFrame {
 		        String result = "";
                 if (BMI < 18.5) {
                     result = "Bạn đang gầy";
-                } else if (BMI >= 18.5 && BMI < 25) {
+                } else if (BMI >= 18.5 && BMI < 22.9) {
                     result = "Bạn có cân nặng bình thường";
-                } else if (BMI >= 25 && BMI < 30) {
+                } else if (BMI == 23) {
                     result = "Bạn đang thừa cân";
                 } else {
                     result = "Bạn đang bị béo phì";
@@ -135,7 +135,7 @@ public class Cau1 extends JFrame {
 		contentPane.add(btnKq);
 		
 		btnXoa = new Button("Xóa");
-		btnXoa.setBounds(322, 227, 107, 42);
+		btnXoa.setBounds(306, 227, 107, 42);
 		btnXoa.setForeground(new Color(0, 0, 0));
 		btnXoa.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
