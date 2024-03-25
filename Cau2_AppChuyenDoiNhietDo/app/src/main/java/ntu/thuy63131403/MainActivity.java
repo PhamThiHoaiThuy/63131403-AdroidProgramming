87a260b85480ce36f88120807493d731719f72e5
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-03-25 09:54:08 +0700

    Update

--- Cau2_AppChuyenDoiNhietDo/app/src/main/java/ntu/thuy63131403/MainActivity.java
+++ Cau2_AppChuyenDoiNhietDo/app/src/main/java/ntu/thuy63131403/MainActivity.java
@@ -3,12 +3,27 @@ package ntu.thuy63131403;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editTextC, editTextK;
+    Button buttonDoC, buttonDoK, buttonXoa;
+    TextView textViewKq;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    void LayDL(){
+        editTextC = findViewById(R.id.edtC);
+        editTextK = findViewById(R.id.edtK);
+        buttonDoC = findViewById(R.id.btnC);
+        buttonDoK = findViewById(R.id.btnK);
+        buttonXoa = findViewById(R.id.btnXoa);
+        textViewKq = findViewById(R.id.txtKq);
+        
     }
 }
