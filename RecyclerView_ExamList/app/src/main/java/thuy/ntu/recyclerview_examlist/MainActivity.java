91c9cdec89c5d2b0ead4f4f244e2567c0ca1c5a5
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-04-17 07:29:59 +0700

    bài thực hành recyclerView_ExamList

---
+++ RecyclerView_ExamList/app/src/main/java/thuy/ntu/recyclerview_examlist/MainActivity.java
@@ -0,0 +1,14 @@
+package thuy.ntu.recyclerview_examlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
