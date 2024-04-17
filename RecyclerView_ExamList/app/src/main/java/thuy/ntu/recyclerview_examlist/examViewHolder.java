48734200b503d1ce6d9ed7dc9f107f0916529b7c
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-04-17 09:32:57 +0700

    hoàn thành bài recyclerView_examList

---
+++ RecyclerView_ExamList/app/src/main/java/thuy/ntu/recyclerview_examlist/examViewHolder.java
@@ -0,0 +1,18 @@
+package thuy.ntu.recyclerview_examlist;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class examViewHolder  extends RecyclerView.ViewHolder {
+    TextView examMessage;
+    TextView examName;
+    TextView examDate;
+    examViewHolder( View itemView) {
+        super(itemView);
+        examName = (TextView)itemView.findViewById(R.id.examName);
+        examDate = (TextView)itemView.findViewById(R.id.examDate);
+        examMessage = (TextView)itemView.findViewById(R.id.examMessage);
+    }
+}
