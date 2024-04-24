2d137333f13500a7c4504524057d8ebb89b81ed4
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-04-24 13:12:49 +0700

    update

--- VD2_Intent_Login/app/src/main/java/thuy/ntu/intent_login/HomeActivity.java
+++ VD2_Intent_Login/app/src/main/java/thuy/ntu/intent_login/HomeActivity.java
@@ -1,4 +1,21 @@
 package thuy.ntu.intent_login;
 
-public class HomeActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+
+    public void QuaylaiTC(View v) {
+        Intent ManHinhChinh = new Intent(this, MainActivity.class);
+        startActivity(ManHinhChinh);
+    }
 }
--- VD2_Intent_Login/app/src/main/java/thuy/ntu/intent_login/LoginActivity.java
+++ VD2_Intent_Login/app/src/main/java/thuy/ntu/intent_login/LoginActivity.java
@@ -1,4 +1,42 @@
 package thuy.ntu.intent_login;
 
-public class LoginActivity {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText Name, Pass, EMail;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        Name = findViewById(R.id.editUserName);
+        Pass = findViewById(R.id.edtPass);
+        EMail = findViewById(R.id.edtMail);
+    }
+
+    public void DangNhap(View v) {
+        String username = Name.getText().toString();
+        String password = Pass.getText().toString();
+        String email = EMail.getText().toString();
+
+        if(username.equals("hoaithuy")&&password.equals("123")&&email.equals("pththuy@gmail.com")){
+            Toast.makeText(this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+        }
+
+        Intent ManHinhHome = new Intent(this, HomeActivity.class);
+        startActivity(ManHinhHome);
+    }
 }
