a3802ce1cb6a7bcc9c837de70702ac039db6a32e
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-04-24 09:59:36 +0700

    Ví Dụ 1 Itent_Explicit

---
+++ VD1_Intent_Explicit/app/src/main/java/thuy/ntu/intent_explicit/MainActivity.java
@@ -0,0 +1,12 @@
+package thuy.ntu.intent_explicit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+}
---
+++ VD1_Intent_Explicit/app/src/main/java/thuy/ntu/intent_explicit/SubOneActivity.java
@@ -0,0 +1,12 @@
+package thuy.ntu.intent_explicit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubOneActivity extends AppCompatActivity {
+
+}
