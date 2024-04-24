6be0120f477722c354383c5bf4173a71ceb5b916
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-04-24 11:19:10 +0700

    Update VD1

--- VD1_Intent_Explicit/app/src/main/java/thuy/ntu/intent_explicit/SubOneActivity.java
+++ VD1_Intent_Explicit/app/src/main/java/thuy/ntu/intent_explicit/SubOneActivity.java
@@ -8,5 +8,20 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SubOneActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subone);
 
+        Button btnQL = findViewById(R.id.btnquaylai);
+
+        btnQL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chuyenTC = new Intent(SubOneActivity.this, MainActivity.class);
+
+                startActivity(chuyenTC);
+            }
+        });
+    }
 }
